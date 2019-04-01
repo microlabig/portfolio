e23d69e3d0b5e4b60c5fe21d908827d81ce9cd55
Igor Bezmestin <igor-rock@list.ru>
2019-04-01 10:11:54 +0300

    Сверстал и отстилизовал блок логина

--- src/admin/App.vue
+++ src/admin/App.vue
@@ -1,5 +1,26 @@
 <template lang="pug">
   .wrapper.wrapper--admin
+    
+    .login
+      .login__container
+        form.login__form
+          .login__close
+            button(type="button").login__close-btn
+          .login__title-box
+            h1.login__title Авторизация
+          .form__wrapper
+            .form__row           
+              lavel.form__elem(data-text="Логин")
+                .form__elem-container.form__elem-container--user
+                  input(type="text" placeholder="Terminator_2000").form__elem-input
+            .form__row
+              lavel.form__elem(data-text="Пароль")
+                .form__elem-container.form__elem-container--key
+                  input(type="password" placeholder="••••••••••").form__elem-input
+            .form__row
+              lavel.form__elem.form__elem--button
+                button(type="submit").form__elem-button Отправить
+
     header.header
       .header__wrapper
         .header__logo
